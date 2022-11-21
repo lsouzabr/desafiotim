9a0ae3dab7a4f8c0feb1d05f315d69fae385e416
Leonam Souza <leonam@prodam.am.gov.br>
2022-11-21 12:43:30 -0400

    requisitos finalizados, iniciando banco não relacional

--- main.go
+++ main.go
@@ -1,18 +1,85 @@
-
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+    "encoding/json"
+    "log"
+    "net/http"
+	"strings"
 )
 
+type Person struct {
+    Letters []string
+}
+
+
+func personCreate(w http.ResponseWriter, r *http.Request) {
+    // Declare a new Person struct.
+    var p Person
+
+	var theArray [6]string
+	theArray[0] = "DUHBHB"  
+	theArray[1] = "DUBUHD" 
+	theArray[2] = "UBUUHU"  
+	theArray[3] = "BHBDHH"  
+	theArray[4] = "DDDDUB" 
+	theArray[5] = "UDBDUH"  
+
+    err := json.NewDecoder(r.Body).Decode(&p)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+	//fmt.Fprintf(w, "\nPalavras enviadas em: %+v", p.Letters)
+
+		acumula := ""
+
+		for i:=0; i<len(p.Letters); i++{
+			
+
+	
+			
+	
+			if(strings.Contains(p.Letters[i], "B")){
+				acumula += "B"
+			}
+			if(strings.Contains(p.Letters[i], "U")){
+				acumula += "U"
+			}		
+			if(strings.Contains(p.Letters[i], "D")){
+				acumula += "D"
+			}		
+			if(strings.Contains(p.Letters[i], "H")){
+				acumula += "H"
+			}		
+	
+	
+			//fmt.Fprintf(w, "\nLetras encontradas: %+v", acumula)
+			//fmt.Fprintf(w, "\nPalavras encontradas: %+v", itemExists(theArray,"UDBDUH ==>"))	
+	
+		}
+		if(len(acumula)>0){
+			w.Header().Set("Content-Type", "application/json")
+
+			jsonStr := `[{"is_valid":true}]`
+		
+			w.Write([]byte(jsonStr))
+		} else{
+			w.Header().Set("Content-Type", "application/json")
+
+			jsonStr := `[{"is_valid":false}]`
+		
+			w.Write([]byte(jsonStr))
+		}
+
+}
+
+
+
 func main() {
-	r := gin.New()
+    mux := http.NewServeMux()
+    mux.HandleFunc("/person/create", personCreate)
 
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello eeeeeeWorld")
-	})
-
-	r.Run()
+    err := http.ListenAndServe(":8080", mux)
+    log.Fatal(err)
 }
